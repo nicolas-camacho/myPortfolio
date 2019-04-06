5c9cd47d5604862468cd3a78a18c389b79388ed7
nicolas-camacho <ncamacho1@cuc.edu.co>
2019-04-05 21:33:37 -0500

    Initial commit with the base of the project

---
+++ src/components/Name.vue
@@ -0,0 +1,41 @@
+<template>
+	<section class="hero is-fullheight nameSection">
+		<div class="hero-body">
+			<div class="container">
+				<div class="content">
+					<h1 class="title is-size-2 has-text-white is-family-monospace">{{myName}}</h1>
+					<h1 class="subtitle is-size-4 vueColor is-family-code is-italic">
+						{{description}}
+					</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			myName: 'Nicolas Rafael Camacho Aguilar',
+			description: "I'm a junior web developer with knowledge in technologies like Vue and the basics of web like HTML, CSS and offcourse javascript"
+		}
+	}
+}
+</script>
+
+<style scoped>
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.nameSection {
+		background-color: #34495E;
+	}
+
+	.vueColor {
+		color: #41B883;
+	}
+</style>
